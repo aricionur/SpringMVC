65f68438d559ba1c42e26c003310466b4495d9b5
Onur Arıcı <aricionur@gmail.com>
2018-12-28 17:28:52 +0300

    hello world example

---
+++ SpringMVCProject/src/com/aricionur/springmvc/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.aricionur.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+
+		model.addAttribute("message", "Spring MVC Hello World");
+		return "hello";  //InternalResourceViewResolver matches to hello.jsp
+
+	}
+	
+}
