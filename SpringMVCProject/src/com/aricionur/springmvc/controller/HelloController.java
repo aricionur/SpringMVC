0d621588ed83268ce6ae3f6325cb1e851454bee4
Onur Arıcı <aricionur@gmail.com>
2019-01-03 09:52:36 +0300

    addin @RequestParam annotation

--- SpringMVCProject/src/com/aricionur/springmvc/controller/HelloController.java
+++ SpringMVCProject/src/com/aricionur/springmvc/controller/HelloController.java
@@ -1,5 +1,17 @@
 package com.aricionur.springmvc.controller;
 
+/* Used annotations:
+ * @Controller		
+ * @RequestMapping
+ * @Scope
+ * @Autowired
+*		@Inject
+*@PathVariable
+*@RequestParam
+*		
+*/
+import javax.inject.Inject;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -7,46 +19,53 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.aricionur.springmvc.service.HelloServicePrototype;
 import com.aricionur.springmvc.service.HelloServiceSingleton;
 
 @Controller
 @RequestMapping("/hello")
-//@Scope("session")
+@Scope("session")
 public class HelloController {
 
-	@Autowired
+//	@Autowired
+	@Inject
 	HelloServiceSingleton helloServiceSingleton;
-	
-	@Autowired
+
+//	@Autowired
+	@Inject
 	HelloServicePrototype helloServicePrototype;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String doHello(ModelMap model) {
 
 		model.addAttribute("message", "Spring MVC Hello World");
-		return "hello";  //InternalResourceViewResolver matches to hello.jsp
+		return "hello"; // InternalResourceViewResolver matches to hello.jsp
 	}
-	
+
 	@RequestMapping(value = "/services/{param1}", method = RequestMethod.GET)
 	public String doHelloServices(ModelMap model, @PathVariable("param1") String param1) {
-		helloServiceSingleton.setMessage(param1 + "singletonservice");
-		helloServicePrototype.setMessage(param1 + "prototypeservice");
-		
+		helloServiceSingleton.setMessage(param1 + "singletonService");
+		helloServicePrototype.setMessage(param1 + "prototypeService");
+
 		model.addAttribute("message_1", helloServiceSingleton.getMessage());
 		model.addAttribute("message_2", helloServicePrototype.getMessage());
-		
-		return "helloServices";  //InternalResourceViewResolver matches to helloServices.jsp
-	}	
-	
-	@RequestMapping(value = "/serviceTest",method = RequestMethod.GET)
+
+		return "helloServices"; // InternalResourceViewResolver matches to helloServices.jsp
+	}
+
+	@RequestMapping(value = "/redirect", method = RequestMethod.GET)
+	public String redirectToPage(@RequestParam("page") String pageName, ModelMap model) {
+		return pageName;
+	}
+
+	@RequestMapping(value = "/serviceTest", method = RequestMethod.GET)
 	public String doServiceTest(ModelMap model) {
 		model.addAttribute("singletonServiceMessage", helloServiceSingleton.getMessage());
 		model.addAttribute("prototypeServiceMessage", helloServicePrototype.getMessage());
 
 		return "testService";
 	}
-	
-	
+
 }
